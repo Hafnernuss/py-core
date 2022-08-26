6438fea96956caa84142ffdef6facf8acf883344
Philipp Hafner <phi.hafner@gmail.com>
2022-08-26 09:37:15 +0200

    initial setup files and requirements

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+  name='py_core',
+  packages=find_packages('.'),
+  install_requires=[
+    'click>=8.1.3',
+    'python-decouple>=3.6'
+    ],
+  entry_points={
+    'console_scripts' : [
+      'cump_conf = py_core.config.dump_config:cli',
+    ]
+  }
+)
